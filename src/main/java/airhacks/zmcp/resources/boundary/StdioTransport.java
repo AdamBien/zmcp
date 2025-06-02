e36056a05a7ab76ae8192ca1a335a45745741964
Adam Bien <abien@adam-bien.com>
2025-06-02 20:41:52 +0200

    multiline strings introduced

--- src/main/java/airhacks/zmcp/resources/boundary/StdioTransport.java
+++ src/main/java/airhacks/zmcp/resources/boundary/StdioTransport.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import airhacks.zmcp.resources.control.ResourceAcces;
 import airhacks.zmcp.resources.entity.Resource;
@@ -58,22 +55,21 @@ public class StdioTransport {
     }
 
     private void handleInitialize(Integer id) {
-        var response = new HashMap<String, Object>();
-        response.put("jsonrpc", "2.0");
-        response.put("id", id);
-        
-        var result = new HashMap<String, Object>();
-        var serverInfo = new HashMap<String, String>();
-        serverInfo.put("name", "zmcp");
-        serverInfo.put("version", "0.0.1");
-        result.put("serverInfo", serverInfo);
-        
-        var capabilities = new HashMap<String, Object>();
-        capabilities.put("resources", new HashMap<>());
-        result.put("capabilities", capabilities);
-        
-        response.put("result", result);
-        writer.println(toJson(response));
+        var response = """
+            {
+                "jsonrpc": "2.0",
+                "id": %d,
+                "result": {
+                    "serverInfo": {
+                        "name": "zmcp",
+                        "version": "0.0.1"
+                    },
+                    "capabilities": {
+                        "resources": {}
+                    }
+                }
+            }""".formatted(id);
+        writer.println(response);
     }
 
     private void handleListResources(Integer id) {
@@ -82,15 +78,15 @@ public class StdioTransport {
             .map(Resource::toJson)
             .collect(Collectors.joining(","));
             
-        var response = new HashMap<String, Object>();
-        response.put("jsonrpc", "2.0");
-        response.put("id", id);
-        
-        var result = new HashMap<String, Object>();
-        result.put("resources", resources);
-        response.put("result", result);
-        
-        writer.println(toJson(response));
+        var response = """
+            {
+                "jsonrpc": "2.0",
+                "id": %d,
+                "result": {
+                    "resources": [%s]
+                }
+            }""".formatted(id, resourcesJson);
+        writer.println(response);
     }
 
     private void handleReadResource(Integer id) {
@@ -106,16 +102,16 @@ public class StdioTransport {
     }
 
     private void sendError(Integer id, int code, String message) {
-        var response = new HashMap<String, Object>();
-        response.put("jsonrpc", "2.0");
-        response.put("id", id);
-        
-        var error = new HashMap<String, Object>();
-        error.put("code", code);
-        error.put("message", message);
-        response.put("error", error);
-        
-        writer.println(toJson(response));
+        var response = """
+            {
+                "jsonrpc": "2.0",
+                "id": %s,
+                "error": {
+                    "code": %d,
+                    "message": "%s"
+                }
+            }""".formatted(id == null ? "null" : id, code, message);
+        writer.println(response);
     }
 
     private String extractMethod(String jsonRequest) {
@@ -135,31 +131,5 @@ public class StdioTransport {
         var end = jsonRequest.indexOf(",", start);
         if (end == -1) end = jsonRequest.indexOf("}", start);
         return Integer.parseInt(jsonRequest.substring(start, end).trim());
-    }
-
-    private String toJson(Map<String, Object> map) {
-        var sb = new StringBuilder();
-        sb.append("{");
-        var entries = map.entrySet().stream()
-            .map(e -> "\"" + e.getKey() + "\":" + valueToJson(e.getValue()))
-            .collect(Collectors.joining(","));
-        sb.append(entries);
-        sb.append("}");
-        return sb.toString();
-    }
-
-    private String valueToJson(Object value) {
-        if (value == null) return "null";
-        if (value instanceof String) return "\"" + value + "\"";
-        if (value instanceof Number) return value.toString();
-        if (value instanceof Boolean) return value.toString();
-        if (value instanceof Map) return toJson((Map<String, Object>) value);
-        if (value instanceof Iterable) {
-            var items = StreamSupport.stream(((Iterable<?>) value).spliterator(), false)
-                .map(this::valueToJson)
-                .collect(Collectors.joining(","));
-            return "[" + items + "]";
-        }
-        return "\"" + value.toString() + "\"";
     }
 } 
