6131cfb6fa46e6b9f496b73175c4d56ce55a6d1a
Adam Bien <abien@adam-bien.com>
2025-06-04 07:14:00 +0200

    ping message implemented

--- src/main/java/airhacks/zmcp/resources/entity/ResourceResponses.java
+++ src/main/java/airhacks/zmcp/resources/entity/ResourceResponses.java
@@ -3,58 +3,59 @@ package airhacks.zmcp.resources.entity;
 public interface ResourceResponses {
     static String initialize(Integer id) {
         return """
-            {
-                "jsonrpc": "2.0",
-                "id": %d,
-                "result": {
-                    "serverInfo": {
-                        "name": "zmcp",
-                        "version": "0.0.1"
-                    },
-                    "capabilities": {
-                        "resources": {
-                            "list": true,
-                            "read": true,
-                            "subscribe": true,
-                            "unsubscribe": true,
-                            "maxSize": 1048576,
-                            "supportedMimeTypes": [
-                                "text/plain",
-                                "text/markdown",
-                                "application/json"
-                            ]
+                {
+                    "jsonrpc": "2.0",
+                    "id": %d,
+                    "result": {
+                        "serverInfo": {
+                            "name": "zmcp",
+                            "version": "0.0.1"
                         },
-                        "roots": {
-                            "list": true,
-                            "listChanged": true
-                        },
-                        "sampling": {
-                            "enabled": true
+                        "capabilities": {
+                            "resources": {
+                                "list": true,
+                                "read": true,
+                                "subscribe": true,
+                                "unsubscribe": true,
+                                "maxSize": 1048576,
+                                "supportedMimeTypes": [
+                                    "text/plain",
+                                    "text/markdown",
+                                    "application/json"
+                                ]
+                            },
+                            "roots": {
+                                "list": true,
+                                "listChanged": true
+                            },
+                            "sampling": {
+                                "enabled": true
+                            }
                         }
                     }
-                }
-            }"""
-            .formatted(id);
-    }
-
-    static String initialized() {
-        return """
-            {
-                "jsonrpc": "2.0",
-                "method": "initialized",
-                "params": {}
-            }""";
+                }"""
+                .formatted(id);
     }
 
     static String listResources(Integer id, String resourcesJson) {
         return """
-            {
-                "jsonrpc": "2.0",
-                "id": %d,
-                "result": {
-                    "resources": [%s]
-                }
-            }"""
-            .formatted(id, resourcesJson);
+                {
+                    "jsonrpc": "2.0",
+                    "id": %d,
+                    "result": {
+                        "resources": [%s]
+                    }
+                }"""
+                .formatted(id, resourcesJson);
     }
-} 
+
+    static String ping(int id) {
+        return """
+                {
+                    "jsonrpc": "2.0",
+                    "id": "%d",
+                    "method": "ping"
+                }"""
+                .formatted(id);
+    }
+}
