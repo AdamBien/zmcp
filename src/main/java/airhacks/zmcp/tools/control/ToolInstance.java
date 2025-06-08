65a66f67c9a0723c9ba9b35f68ec5d7ac25dea74
Adam Bien <abien@adam-bien.com>
2025-06-08 05:38:43 +0200

    initial tool registry implementation

---
+++ src/main/java/airhacks/zmcp/tools/control/ToolInstance.java
@@ -0,0 +1,34 @@
+package airhacks.zmcp.tools.control;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+import airhacks.zmcp.log.boundary.Log;
+import airhacks.zmcp.tools.api.ToolInvocation;
+import airhacks.zmcp.tools.api.ToolSpec;
+
+public record ToolInstance(ToolInvocation tool, String name, String description, String inputSchema) {
+    
+    public static Optional<ToolInstance> of(ToolInvocation tool) {
+        var toolClass = tool.getClass();
+        Method toolMethod;
+        try {
+            toolMethod = toolClass.getMethod("invoke", String.class);
+        } catch (NoSuchMethodException e) {
+            Log.error("Tool " + toolClass.getName() + " does not have an invoke method");
+            return Optional.empty();
+        }
+
+        var toolSpec = toolMethod.getAnnotation(ToolSpec.class);
+        
+        var name = toolSpec.name();
+        var description = toolSpec.description();
+        var inputSchema = toolSpec.inputSchema();
+        var toolDescription = new ToolInstance(tool, name, description, inputSchema);
+        return Optional.of(toolDescription);
+    }
+
+
+    
+
+}
---
+++ src/main/java/airhacks/zmcp/tools/control/ToolsRegistry.java
@@ -0,0 +1,23 @@
+package airhacks.zmcp.tools.control;
+
+import java.util.Optional;
+import java.util.ServiceLoader;
+import java.util.ServiceLoader.Provider;
+
+import airhacks.zmcp.tools.api.ToolInvocation;
+
+public interface ToolsRegistry {
+
+    static Optional<ToolInstance> findTool(String name) {
+        return ServiceLoader.load(ToolInvocation.class)
+                .stream()
+                .map(Provider::get)
+                .map(ToolInstance::of)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst();
+
+    }
+
+}
+ 
