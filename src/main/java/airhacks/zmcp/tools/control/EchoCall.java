c57f014e861fc1a8b56af5f9a34eeca8c9c37411
Adam Bien <abien@adam-bien.com>
2025-06-09 05:40:00 +0200

    echo call test tool invocation with configuration

---
+++ src/main/java/airhacks/zmcp/tools/control/EchoCall.java
@@ -0,0 +1,16 @@
+package airhacks.zmcp.tools.control;
+
+import airhacks.zmcp.log.boundary.Log;
+import airhacks.zmcp.tools.api.ToolInvocation;
+import airhacks.zmcp.tools.api.ToolSpec;
+
+import java.util.Optional;
+
+public class EchoCall implements ToolInvocation {
+    
+    @ToolSpec(name = "echo", description = "Echo the input, useful for testing", inputSchema = "string")
+    public Optional<String> invoke(String parameters) {
+        Log.info("echoing: " + parameters);
+        return Optional.of("Echo: " + parameters);
+    }
+}
