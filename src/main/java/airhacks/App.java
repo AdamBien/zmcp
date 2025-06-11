41473d70877b0b157acc2cc6179575804a6bf4cd
Adam Bien <abien@adam-bien.com>
2025-06-11 07:25:51 +0200

    used object as input schema

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -15,7 +15,7 @@ import airhacks.zmcp.tools.boundary.ToolsSTDIOProtocol;
  */
 public interface App {
 
-    String VERSION = "zmcp v2025.06.10.08.06";     
+    String VERSION = "zmcp v2025.06.11.01";     
 
 
     static void main(String... args) throws IOException {
