3fca5485d5734dd09d00ed02e102e25f9f69f8fe
Adam Bien <abien@adam-bien.com>
2025-06-23 21:32:23 +0200

    UT for error response

---
+++ src/test/java/airhacks/zmcp/jsonrpc/entity/ErrorResponsesTest.java
@@ -0,0 +1,26 @@
+package airhacks.zmcp.jsonrpc.entity;
+
+import org.junit.jupiter.api.Test;
+
+import airhacks.zmcp.JSONAssertions;
+
+
+public class ErrorResponsesTest {
+
+    @Test
+    void error() {
+
+        var error = ErrorResponses.error(1, -32602, "Invalid params");
+        var expected = """
+                {
+                    "jsonrpc": "2.0",
+                    "id": 1,
+                    "error": {
+                        "code": -32602,
+                        "message": "Invalid params"
+                    }
+                }""";
+        JSONAssertions.assertEquals(error, expected);
+
+    }
+}
