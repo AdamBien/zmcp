a590159ba037c9543c1490fd680ed8824c6f2a19
Adam Bien <abien@adam-bien.com>
2025-06-24 09:02:13 +0200

    arguments parsed

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -18,12 +18,26 @@ public interface App {
 
     String VERSION = "zmcp v2025.06.23.01";     
 
+    String RESOURCE_DIR =".";
+    String PROMPTS_DIR = "./prompts";
+
+    record Arguments(String resourceDir,String promptsDir){
+
+        static Arguments from(String... args){
+            return new Arguments(
+                args.length > 0 ? args[0]: RESOURCE_DIR,
+                args.length > 1 ? args[1] : PROMPTS_DIR);
+              
+        }
+    }
+
     static void main(String... args) throws IOException {
         Log.init();
+        var arguments = Arguments.from(args);
         var frontDoor = new FrontDoor(List.of(
-            new ResourcesSTDIOProtocol("."),
+            new ResourcesSTDIOProtocol(arguments.resourceDir()),
             new ToolsSTDIOProtocol(),
-            new PromptsSTDIOProtocol("./prompts")));
+            new PromptsSTDIOProtocol(arguments.promptsDir())));
         frontDoor.start();
     }
 }
